4d4499a4c87448885df56e57534d085e3c4c217f
xeli <akseli.palen@gmail.com>
2015-10-29 15:33:49 +0200

    Improved docs & connection error handling.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Gazelle Database
 
+Open database for scientific gaze data.
+
 Akseli Palén
 Infant Cognition Laboratory
 University of Tampere
@@ -12,14 +14,24 @@ University of Tampere
     (env)$ pip install flask
     (env)$ pip install pymongo
     (env)$ pip install numpy
-	(env)$ pip install coloredlogs
+    (env)$ pip install coloredlogs
 
 ## Run
+
+First, start MongoDB database server:
 
     $ cd project/root/path
     $ mongod --config data/mongodb.conf
 
+Then, activate the virtual Python environment and start the web server:
+
+    $ source bin/activate
     (env)$ python gazelledb/server.py
+
+If you want to shut down the web server, close it and exit the environment:
+
+    [press ctrl-c]
+    (env)$ deactivate
 
 Optionally you can also open Mongo console:
 
@@ -30,82 +42,84 @@ Optionally you can also open Mongo console:
 Trial gaze points
 
 {
-	'name': name_root + '/',
-	'timestamp': 123456789,
-	'format': 'gazelle/v1/trial',
-	'meta': {
-		'date': gazedata.get_trial_date(trial),
-		'participant_id': meta['participant_id'],
-		'method_version': meta['method_version'],
-		'participant_age_months': meta['participant_age_months'],
-		'calibration_successful': meta['calibration_successful'],
-		'trial_configuration_id': meta['trial_configuration_id'],
-		'trial_number': trial_num,
-		'aoi_x_rel': aoi_xy[0],
-		'aoi_y_rel': aoi_xy[1],
-		'source_file': file,
-		'target': {
-			'range': [100, -1]
-			'meta': {
-				...
-			}
-		},
-		'first_and_second': {
-			'include': [0, 1]
-		},
-		'last': {
-			'include': [-1]
-		},
-		'saccade': {
-			'range': [678, 789]
-		}
-	},
-	'function': [],
-	'input': [],
-	'input_timestamp': [],
-	'output': norm_g
+    'name': name_root + '/',
+    'timestamp': 123456789,
+    'format': 'gazelle/v1/trial',
+    'meta': {
+        'date': gazedata.get_trial_date(trial),
+        'participant_id': meta['participant_id'],
+        'method_version': meta['method_version'],
+        'participant_age_months': meta['participant_age_months'],
+        'calibration_successful': meta['calibration_successful'],
+        'trial_configuration_id': meta['trial_configuration_id'],
+        'trial_number': trial_num,
+        'aoi_x_rel': aoi_xy[0],
+        'aoi_y_rel': aoi_xy[1],
+        'source_file': file,
+        'tags': {
+            'target': {
+                'range': [100, -1]
+                'meta': {
+                    ...
+                }
+            },
+            'first_and_second': {
+                'include': [0, 1]
+            },
+            'last': {
+                'include': [-1]
+            },
+            'saccade': {
+                'range': [678, 789]
+            }
+        }
+    },
+    'function': [],
+    'input': [],
+    'input_timestamp': [],
+    'output': norm_g
 }
 
 Trial analysis
 
 {
-	'name': icl/trial/analysis,
-	'timestamp': 1233456789,
-	'format': 'gazelle/v1/saccade', // Description
-	'meta': {},
-	'function': ['analysis.py'],
-	'input': string or list or pattern
-		['icl/name@123456789', '...@...', 'name@time']
-	'input_timestamp': { // Check if newer
-		'icl/name@123456789': 123456789,
-		'icl/name2': 123456788
-	}// or list, keys are in the input
-	'output': a list in format gazelle/v1/saccade
+    'name': icl/trial/analysis,
+    'timestamp': 1233456789,
+    'format': 'gazelle/v1/saccade', // Description
+    'meta': {},
+    'function': ['analysis.py'],
+    'input': string or list or pattern
+        ['icl/name@123456789', '...@...', 'name@time']
+    'input_timestamp': { // Check if newer
+        'icl/name@123456789': 123456789,
+        'icl/name2': 123456788
+    }// or list, keys are in the input
+    'output': a list in format gazelle/v1/saccade
 }
 
 Trial analysis merging
 
 {
-	'name': 'icl/sequence',
-	'timestamp': 1233456789,
-	'format': 'gazelle/v1/saccades',
-	'meta': {},
-	'function': 'merge-analysis.py',
-	'input': ['icl/trial', 'icl/trial2'],
-	'output': list in format gazelle/v1/saccades
+    'name': 'icl/sequence',
+    'timestamp': 1233456789,
+    'format': 'gazelle/v1/saccades',
+    'meta': {},
+    'function': 'merge-analysis.py',
+    'input': ['icl/trial', 'icl/trial2'],
+    'output': list in format gazelle/v1/saccades
 }
 
 Prediction analysis
 
 {
-	'name': ...,
-	'timestamp': 11111111111,
-	'format': 'gazelle/v1/prediction',
-	'meta': {},
-	'function': 'prediction-analysis.py',
-	'input': ['icl/sequence'],
-	'input_timestamp': [1234567678],
-	'output': {
-		'probabilities': ... how they affect reaction time, LATER model
-	}
+    'name': ...,
+    'timestamp': 11111111111,
+    'format': 'gazelle/v1/prediction',
+    'meta': {},
+    'function': 'prediction-analysis.py',
+    'input': ['icl/sequence'],
+    'input_timestamp': [1234567678],
+    'output': {
+        'probabilities': ... how they affect reaction time, LATER model
+    }
 }
--- gazelledb/server.py
+++ gazelledb/server.py
@@ -3,8 +3,12 @@ from flask import Flask, jsonify, request, abort, render_template
 app = Flask(__name__)
 
 from pymongo import MongoClient
-from pymongo.errors import DuplicateKeyError
-mongo = MongoClient()
+from pymongo.errors import DuplicateKeyError, ServerSelectionTimeoutError
+
+# Database connection setup.
+# Defaults
+#    serverSelectionTimeoutMS=30000
+mongo = MongoClient(serverSelectionTimeoutMS=5000)
 db = mongo.gazelle
 
 import json
@@ -13,134 +17,149 @@ from bson import json_util
 from lib import get_node
 from lib import create_node
 
+def db_connection_failure(abort_fn, e):
+    print 'Connection to database could not be established: ' + e.message
+    abort_fn(500)
+    return
 
 @app.route('/')
 def front():
-	cursor = db.nodes.find(projection=['name']).limit(20)
-	node_names = [x['name'] for x in cursor]
-	return render_template('index.html', node_names=node_names)
+    try:
+        cursor = db.nodes.find(projection=['name']).limit(20)
+        node_names = [x['name'] for x in cursor]
+        return render_template('index.html', node_names=node_names)
+    except ServerSelectionTimeoutError as e:
+        return db_connection_failure(abort, e)
 
 @app.route('/test/saccadeem/')
 def tester():
-	# Compare
-	matlab_results = {
-		'icl/cg/person/0001/age/8mo/method/8/srt1/trial/02/': {
-			't_start': 137, 't_end': 156,
-			'mse': 0.0017, 'srt': 456, 'sd': 63,
-			'em_iterations': 5,
-			'source_error': 0.0369,
-			'saccade_error': 0.0183,
-			'target_error': 0.4728,
-		},
-		'icl/cg/person/0001/age/8mo/method/8/srt1/trial/04/': {
-			't_start': 78, 't_end': 92,
-			'mse': 0.0070, 'srt': 260, 'sd': 46,
-			'em_iterations': 4,
-			'source_error': 0.0279,
-			'saccade_error': 0.0153,
-			'target_error': 2.0708,
-		},
-		'icl/cg/person/0001/age/8mo/method/8/srt2/trial/07/': {
-			't_start': 115, 't_end': 130,
-			'mse': 0.0023, 'srt': 383, 'sd': 50,
-			'em_iterations': 3,
-			'source_error': 0.0622,
-			'saccade_error': 0.0191,
-			'target_error': 0.6249,
+    # Compare
+    matlab_results = {
+        'icl/cg/person/0001/age/8mo/method/8/srt1/trial/02/': {
+            't_start': 137, 't_end': 156,
+            'mse': 0.0017, 'srt': 456, 'sd': 63,
+            'em_iterations': 5,
+            'source_error': 0.0369,
+            'saccade_error': 0.0183,
+            'target_error': 0.4728,
+        },
+        'icl/cg/person/0001/age/8mo/method/8/srt1/trial/04/': {
+            't_start': 78, 't_end': 92,
+            'mse': 0.0070, 'srt': 260, 'sd': 46,
+            'em_iterations': 4,
+            'source_error': 0.0279,
+            'saccade_error': 0.0153,
+            'target_error': 2.0708,
+        },
+        'icl/cg/person/0001/age/8mo/method/8/srt2/trial/07/': {
+            't_start': 115, 't_end': 130,
+            'mse': 0.0023, 'srt': 383, 'sd': 50,
+            'em_iterations': 3,
+            'source_error': 0.0622,
+            'saccade_error': 0.0191,
+            'target_error': 0.6249,
 
-		},
-		'icl/cg/person/0001/age/8mo/method/8/srt4/trial/09/': {
-			't_start': 95, 't_end': 109,
-			'mse': 0.0024, 'srt': 316, 'sd': 46,
-			'em_iterations': 3,
-			'source_error': 0.3362,
-			'saccade_error': 0.0441,
-			'target_error': 0.3500,
-		},
-		'icl/cg/person/0002/age/8mo/method/8/srt1/trial/00/': {
-			't_start': 96, 't_end': 130,
-			'mse': 0.0223, 'srt': 320, 'sd': 113,
-			'em_iterations': 3,
-			'source_error': 3.3583,
-			'saccade_error': 0.1351,
-			'target_error': 3.2047,
-		},
-		'icl/cg/person/0002/age/8mo/method/8/srt1/trial/05/': {
-			't_start': 15, 't_end': 137,
-			'mse': 0.0359, 'srt': 50, 'sd': 406,
-			'em_iterations': 4,
-			'source_error': 0.0000,
-			'saccade_error': 10.1766,
-			'target_error': 0.5995,
-		},
-	}
+        },
+        'icl/cg/person/0001/age/8mo/method/8/srt4/trial/09/': {
+            't_start': 95, 't_end': 109,
+            'mse': 0.0024, 'srt': 316, 'sd': 46,
+            'em_iterations': 3,
+            'source_error': 0.3362,
+            'saccade_error': 0.0441,
+            'target_error': 0.3500,
+        },
+        'icl/cg/person/0002/age/8mo/method/8/srt1/trial/00/': {
+            't_start': 96, 't_end': 130,
+            'mse': 0.0223, 'srt': 320, 'sd': 113,
+            'em_iterations': 3,
+            'source_error': 3.3583,
+            'saccade_error': 0.1351,
+            'target_error': 3.2047,
+        },
+        'icl/cg/person/0002/age/8mo/method/8/srt1/trial/05/': {
+            't_start': 15, 't_end': 137,
+            'mse': 0.0359, 'srt': 50, 'sd': 406,
+            'em_iterations': 4,
+            'source_error': 0.0000,
+            'saccade_error': 10.1766,
+            'target_error': 0.5995,
+        },
+    }
 
-	diffs = {}
+    diffs = {}
 
-	for n, o in matlab_results.iteritems():
-		n = n + 'saccade/'
-		node = get_node.by_name_computed(db, n)
-		if node is None:
-			diffs[n] = 'not found'
-		else:
-			if cmp(node['output'], o) == 0:
-				diffs[n] = 'match'
-			else:
-				diffs[n] = {
-					'expected': o,
-					'was': node['output']
-				}
+    for n, o in matlab_results.iteritems():
+        n = n + 'saccade/'
+        try:
+            node = get_node.by_name_computed(db, n)
+        except ServerSelectionTimeoutError as e:
+            return db_connection_failure(abort, e)
+        if node is None:
+            diffs[n] = 'not found'
+        else:
+            if cmp(node['output'], o) == 0:
+                diffs[n] = 'match'
+            else:
+                diffs[n] = {
+                    'expected': o,
+                    'was': node['output']
+                }
 
-	return json.dumps(diffs)
+    return json.dumps(diffs)
 
 @app.route('/<path:node_name>', methods=['GET'])
 def node(node_name):
-	print 'HTTP GET ' + node_name
-	try:
-		r = get_node.by_name_computed(db, node_name, policy='init')
-	except:
-		import traceback
-		traceback.print_exc()
-		abort(500)
-	if r is None:
-		abort(404)
-	else:
-		# Remove internal data from response
-		r.pop('_id', None)
-		return json_util.dumps(r)
+    print 'HTTP GET ' + node_name
+    try:
+        r = get_node.by_name_computed(db, node_name, policy='init')
+    except ServerSelectionTimeoutError as e:
+        return db_connection_failure(abort, e)
+    except:
+        import traceback
+        traceback.print_exc()
+        abort(500)
+        return
+    if r is None:
+        abort(404)
+    else:
+        # Remove internal data from response
+        r.pop('_id', None)
+        return json_util.dumps(r)
 
 @app.route('/<path:node_name>', methods=['PUT'])
 def insert_node(node_name):
-	print 'HTTP PUT ' + node_name
+    print 'HTTP PUT ' + node_name
 
-	# Note: aborts with 400 if malformed JSON
-	j = request.get_json()
+    # Note: aborts with 400 if malformed JSON
+    j = request.get_json()
 
-	if 'name' in j:
-		if node_name != j['name']:
-			abort(400)
+    if 'name' in j:
+        if node_name != j['name']:
+            abort(400)
 
-	try:
-		new_node = create_node.by_prototype(j)
-		db.nodes.insert_one(new_node)
-	except DuplicateKeyError:
-		return 'Name already taken: ' + node_name, 400
-	except create_node.InvalidNodeCandidate as e:
-		return str(e), 400
-	except:
-		import traceback
-		traceback.print_exc()
-		abort(500)
+    try:
+        new_node = create_node.by_prototype(j)
+        db.nodes.insert_one(new_node)
+    except ServerSelectionTimeoutError as e:
+        return db_connection_failure(abort, e)
+    except DuplicateKeyError:
+        return 'Name already taken: ' + node_name, 400
+    except create_node.InvalidNodeCandidate as e:
+        return str(e), 400
+    except:
+        import traceback
+        traceback.print_exc()
+        abort(500)
 
-	return json_util.dumps(new_node), 200
+    return json_util.dumps(new_node), 200
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return render_template('error404.html'), 404
+    return render_template('error404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(error):
-	return render_template('error500.html'), 500
+    return render_template('error500.html'), 500
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
